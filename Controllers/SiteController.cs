95d603a9e910f59efb58813b663cb41409f2c6a6
Dunja Peric <64160013+Dunja259@users.noreply.github.com>
2022-01-13 11:35:10 +0100

    Add files via upload

---
+++ Controllers/SiteController.cs
@@ -0,0 +1,99 @@
+﻿using Projekat.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Projekat.Controllers
+{
+    public class SiteController : Controller
+    {
+        // GET: Site
+        public ActionResult Index()
+        {
+            ViewBag.naslov = "Digital Marketing Agency";
+            ViewBag.tekst = "There is no such thing as a great agency, just great people in agencies. We are a team of online marketing experts with background in top - notch companies such as Google, Accenture and Oracle.";
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            return View();
+        }
+
+        public ActionResult Work()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public ActionResult Contact()
+        {
+            return View();
+        }
+        
+        [HttpPost]
+        public ViewResult Contact(Models.mainModel form)
+        {
+            return View("ContactFormAnswer", form);
+        }
+
+        [HttpGet]
+        public ActionResult Services()
+        {
+            Company[] companies = new Company[]
+           {
+                new Company("Export","Logo"),
+                new Company("Startups", "Logo"),
+                new Company("Online trading voucher", "Logo")
+           };
+            List<string> name = new List<string>();
+            foreach (Company c in companies)
+            {
+                name.Add(c.name);
+            }
+            ViewBag.name = name;
+
+            return View();
+        }
+
+        [HttpPost]
+        public ViewResult Services(Models.mainModel services)
+        {
+            return View("ServiceChoice", services);
+            
+        }
+
+        [HttpGet]
+        public ActionResult Plan()
+        {
+            Company[] companies = new Company[]
+            {
+                new Company("WebFX","Logo"),
+                new Company("Ignite Visibility", "Logo"),
+                new Company("SocialSEO", "Logo"),
+                new Company("Victorious", "Logo")
+            };
+
+            List<string> name = new List<string>();
+            foreach (Company c in companies)
+            {
+                name.Add(c.name);
+            }
+            ViewBag.name = name;
+
+
+            return View();
+        }
+
+        [HttpPost]
+        public ViewResult Plan(Models.mainModel plan)
+        {
+            
+
+            return View("YourPlan", plan);
+        }
+
+    }
+}
